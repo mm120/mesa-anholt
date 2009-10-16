6e320d79d4b456fa9a042c2c52d6c2326f691883
Eric Anholt <eric@anholt.net>
2009-10-16 13:00:01 -0700

    Start splitting out the demos repo: Remove everything but progs.

--- src/gallium/state_trackers/es/st_es1.c
+++
@@ -1,8 +0,0 @@
-#include "state_tracker/st_manager.h"
-
-PUBLIC const int st_api_OpenGL_ES1 = 1;
-
-PUBLIC const struct st_module st_module_OpenGL_ES1 = {
-   .api = ST_API_OPENGL_ES1,
-   .create_api = st_manager_create_api
-};
--- src/gallium/state_trackers/es/st_es2.c
+++
@@ -1,8 +0,0 @@
-#include "state_tracker/st_manager.h"
-
-PUBLIC const int st_api_OpenGL_ES2 = 1;
-
-PUBLIC const struct st_module st_module_OpenGL_ES2 = {
-   .api = ST_API_OPENGL_ES2,
-   .create_api = st_manager_create_api
-};

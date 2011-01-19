a4e31d43ee31ffbff766ba6eb9e76249e569851b
Eric Anholt <eric@anholt.net>
2011-01-18 17:18:01 -0800

    glsl: Drop tree grafting from the common optimization path.
    This pass was somewhat useful in that it would pull together larger
    expression trees, particularly on

--- src/glsl/glsl_parser_extras.cpp
+++ src/glsl/glsl_parser_extras.cpp
@@ -732,7 +732,6 @@ do_common_optimization(exec_list *ir, bool linked, unsigned max_unroll_iteration
    else
       progress = do_dead_code_unlinked(ir) || progress;
    progress = do_dead_code_local(ir) || progress;
-   progress = do_tree_grafting(ir) || progress;
    progress = do_constant_propagation(ir) || progress;
    if (linked)
       progress = do_constant_variable(ir) || progress;

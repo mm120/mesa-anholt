3b05332f9f2e7a666a0e613b7803264be52da24c
Paul Berry <stereotype441@gmail.com>
2012-11-14 14:26:22 -0800

    Add SetBackgroundContext to dd_function_table

--- src/mesa/main/dd.h
+++ src/mesa/main/dd.h
@@ -842,6 +842,25 @@ struct dd_function_table {
                              struct gl_framebuffer *fb,
                              GLuint index,
                              GLfloat *outValue);
+
+   /**
+    * Indicate that this thread is being used by Mesa as a background drawing
+    * thread for the given GL context.
+    *
+    * If this function is called more than once from any given thread, each
+    * subsequent call overrides the context that was passed in the previous
+    * call.  Mesa takes advantage of this to re-use a background thread to
+    * perform drawing on behalf of multiple contexts.
+    *
+    * Mesa may sometimes call this function from a non-background thread
+    * (i.e. a thread that has already been bound to a context using
+    * __DriverAPIRec::MakeCurrent()); when this happens, ctx will be equal to
+    * the context that is bound to this thread.
+    *
+    * Mesa will only call this function if _mesa_enable_multithreading() has
+    * been called on a given context.
+    */
+   void (*SetBackgroundContext)(struct gl_context *ctx);
 };
 
 

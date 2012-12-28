5175464c8eecb12bb7247906c8971b5fa36f6a92
Eric Anholt <eric@anholt.net>
2012-12-28 11:48:01 -0800

    intel: Rely on flush_with_flags for deciding when to throttle.

--- src/mesa/drivers/dri/intel/intel_screen.c
+++ src/mesa/drivers/dri/intel/intel_screen.c
@@ -201,6 +201,8 @@ intel_dri2_flush_with_flags(__DRIcontext *cPriv,
  * 1-3) flush interface.
  *
  * That includes libGL up to Mesa 9.0, and the X Server at least up to 1.13.
+ * The extension has been used in the X Server since 2009, so we can rely on
+ * it being used for swap throttling.
  */
 static void
 intel_dri2_flush(__DRIdrawable *drawable)

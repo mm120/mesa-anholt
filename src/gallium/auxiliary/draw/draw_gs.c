Plombo/mesa

draw/gs: fix allocation of buffer for GS output vertices
Before, it accounted for the size of the vertices but not the other fields
in the vertex_header struct, which caused memory corruption.

src/gallium/auxiliary/draw/draw_gs.c
@@ -412,7 +412,8 @@ int draw_geometry_shader_run(struct draw_geometry_shader *shader,
    output_verts->vertex_size = input_verts->vertex_size;
    output_verts->stride = input_verts->vertex_size;
    output_verts->verts =
-      (struct vertex_header *)MALLOC(input_verts->vertex_size *
+      (struct vertex_header *)MALLOC(sizeof(struct vertex_header) +
+                                     input_verts->vertex_size *
                                      num_in_primitives *
                                      shader->max_output_vertices);
 

mm120/mesa-anholt

src/gallium/auxiliary/draw/draw_gs.c
@@ -560,7 +560,8 @@ int draw_geometry_shader_run(struct draw_geometry_shader *shader,
    /* we allocate exactly one extra vertex per primitive to allow the GS to emit
     * overflown vertices into some area where they won't harm anyone */
    output_verts->verts =
-      (struct vertex_header *)MALLOC(output_verts->vertex_size *
+      (struct vertex_header *)MALLOC(sizeof(struct vertex_header) +
+                                     output_verts->vertex_size *
                                      max_out_prims *
                                      shader->primitive_boundary);
 
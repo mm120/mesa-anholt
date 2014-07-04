e511cefcd33e95541725baa4ab7716d26e0e1a2f
Eric Anholt <eric@anholt.net>
2014-07-04 10:59:42 -0700

    vc4: Add copy propagation between temps.
    We put in a bunch of extra MOVs for program outputs, and this can clean
    those up.  We should do uniforms, too, though.
    
    v2: Fix missing flagging of progress when we actually optimize.  Caught by
        Aaron Watry.

--- src/gallium/drivers/vc4/vc4_qir.h
+++ src/gallium/drivers/vc4/vc4_qir.h
@@ -152,6 +152,7 @@ const char *qir_get_stage_name(enum qstage stage);
 
 void qir_optimize(struct qcompile *c);
 bool qir_opt_algebraic(struct qcompile *c);
+bool qir_opt_copy_propagation(struct qcompile *c);
 bool qir_opt_dead_code(struct qcompile *c);
 
 #endif /* VC4_QIR_H */

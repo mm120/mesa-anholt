0400283cb495e00d5e3d28b44b7f14852daa4b6c
Eric Anholt <eric@anholt.net>
2014-08-02 20:44:39 -0700

    vc4: Fix the shader record size for extended strides.
    It turns out they aren't packed when attributes are missing, according to
    both docs and simulation.

--- src/gallium/drivers/vc4/vc4_simulator_validate.c
+++ src/gallium/drivers/vc4/vc4_simulator_validate.c
@@ -90,11 +90,15 @@ gl_shader_rec_size(uint32_t pointer_bits)
 {
 	uint32_t attribute_count = pointer_bits & 7;
 	bool extended = pointer_bits & 8;
+	uint32_t size;
 
 	if (attribute_count == 0)
 		attribute_count = 8;
 
-	return 36 + attribute_count * (extended ? 12 : 8);
+	if (extended)
+		return 100 + attribute_count * 4;
+	else
+		return 36 + attribute_count * 8;
 }
 
 static bool
